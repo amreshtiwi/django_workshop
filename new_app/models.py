ee382d5dff7eb06ba4582e2913440b72d7475c39
amreshtiwi <ammr64@outlook.com>
2022-05-30 15:48:44 +0300

    second day content

--- new_app/models.py
+++ new_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class trip:
+    id:int
+    name:str
+    image:str
+    descrption:str
+    price:int
 # Create your models here.
--- new_app/views.py
+++ new_app/views.py
@@ -1,5 +1,6 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import trip
 
 # Create your views here.
 def home(request):
@@ -7,7 +8,29 @@ def home(request):
     return render(request , 'home.html',{'name':'Amoora'})
 
 def add(request):
-    val1 =int(request.GET['num1'])
-    val2 = int(request.GET['num2'])
+    val1 =int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
     res=val1+val2
     return render(request,"result.html",{'result': res})
+
+def index(request):
+    trip1 = trip()
+    trip1.name = 'Mumbai'
+    trip1.descrption = 'The city that never sleeps'
+    trip1.image = 'destination_1.jpg'
+    trip1.price = 120
+
+    trip2 = trip()
+    trip2.name = 'Mumbai'
+    trip2.descrption = 'The city that never sleeps'
+    trip2.image = 'destination_2.jpg'
+    trip2.price = 120
+
+    trip3 = trip()
+    trip3.name = 'Mumbai'
+    trip3.descrption = 'The city that never sleeps'
+    trip3.image = 'destination_3.jpg'
+    trip3.price = 120
+
+    trips = [trip1,trip2,trip3]
+    return render(request, "index.html" ,{'trips':trips})
