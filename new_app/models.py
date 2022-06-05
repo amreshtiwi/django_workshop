99cb8f12371fe8a266a67b436f8bf1ea7f2165b3
amreshtiwi <ammr64@outlook.com>
2022-06-05 09:16:04 +0300

    third day of training

--- new_app/models.py
+++ new_app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-class trip:
-    id:int
-    name:str
-    image:str
-    descrption:str
-    price:int
+class trip(models.Model):
+    #id:int
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='pics')
+    descrption = models.TextField()
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
 # Create your models here.
